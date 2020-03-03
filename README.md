a2a8fd61c8b7832efde904deb25f13da2df5b23a
pratiksha1405 <pratikshasingh1405@gmail.com>
2020-03-03 13:02:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-akw6f9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-akw6f9)
---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ 
+  User:any
+  constructor(){
+ this.User = 
+ [{UserId:101,
+  Name:"Ross",
+  City:"Chennai",
+  Age:28,
+  Salary:20000
+ },
+ {UserId:178,
+  Name:"Chandler",
+  City:"Mumbai",
+  Age:29,
+  Salary:15000
+ },
+ {UserId:156,
+ Name:"Joey",
+ City:"Pune",
+ Age:30,
+ Salary:25000
+},
+{UserId:203,
+Name:"Rachel",
+City:"Lucknow",
+Age:25,
+Salary:30000
+},
+{UserId:621,
+Name:"Monica",
+City:"Delhi",
+Age:31,
+Salary:28000
+},
+{UserId:534,
+Name:"Pheobe",
+City:"Kanpur",
+Age:35,
+Salary:19000
+}
+ ]};
+
+
+}
